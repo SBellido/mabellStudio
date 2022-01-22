cb28b622d36e0315c229e74d345b35ffd840fb28
SBellido <sebastianbellidodg@gmail.com>
2022-01-22 00:05:47 -0300

    Working random style in words

--- src/app/works/components/home/home.component.ts
+++ src/app/works/components/home/home.component.ts
@@ -10,16 +10,25 @@ export class HomeComponent implements OnInit {
   concepts: string[] = [
     'aprender','superarse','apertura','perseverancia',
     'creatividad','experiencia','ubicación','respeto',
-    'compromiso','comunicación','escuchar','capacitarse',
+    'compromiso','comunicación','escuchar','práctica',
     'dialogar','intuición','respeto','proyectos','ideas',
     'curiosidad','amor','resultados','equipo','confianza',
     'interés','arriesgar','actitud','formación','atención',
-    'capacitación','confianza','sueños','sinceridad','práctica',
+    'capacitación','confianza','sueños','sinceridad',
     'conocimiento','humildad','empatía','resolución' 
   ];
 
-  weightFont: number[] = [100,200,300,400,500,600,700,800,900];
-
+  weightFont: number[] = [
+    100,200,300,400,
+    500,600,700,800,900
+  ];
+  colorFont: string[] = [
+    '#03caff','#03caff','#7AE3FF',
+    '#f9f9f9','#f9f9f9','#FFDD57','#FFEB9B'
+  ]; 
+  fontStyle: string[] = [
+    'oblique', 'italic','normal'
+  ];
   constructor() { }
 
   ngOnInit(): void {   
@@ -33,11 +42,10 @@ export class HomeComponent implements OnInit {
   getStyles() {
     let myStyles = {
       fontWeight: this.getRandomWeight(),
-      color: 'ivory',
+      color: this.getRandomColor(),
+      fontStyle: this.getRandomFont()
     }
-    return myStyles;
-
-    
+    return myStyles;    
   }
   getRandomWeight(): number {
     let result: number;
@@ -45,4 +53,17 @@ export class HomeComponent implements OnInit {
     result = this.weightFont[0];
     return result;
   }
+  getRandomColor(): string {
+    let result: string;
+    this.colorFont.sort(()=> Math.random() - 0.5); 
+    result = this.colorFont[0];
+    return result;
+  }
+  getRandomFont(): string {
+    let result: string;
+    this.fontStyle.sort(()=> Math.random() - 0.5); 
+    result = this.fontStyle[0];
+    return result;
+  }
+  
 }
