52eb81b32025299e93b8d10a4c47e1bef4ebb088
Sebastian.Bellido <sebastianbellidodg@gmail.com>
2022-09-23 19:28:23 -0300

    Working in loboral biography

--- src/app/works/components/biography/biography.component.ts
+++ src/app/works/components/biography/biography.component.ts
@@ -10,7 +10,7 @@ export class BiographyComponent implements OnInit {
 
   visibleLabor: boolean = false;
   visibleAcademic: boolean = false;
-
+  
   constructor(private router: Router) { }
 
   ngOnInit(): void {  
