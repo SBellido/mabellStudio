93f9e1090af3635b969b33ec71e2b57c3a27b484
SBellido <sebastianbellidodg@gmail.com>
2023-06-21 19:43:24 +0200

    Stable point, working in pipe to resalt some words

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,30 @@
-/* Angular */
+// /* Angular */
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { AppRoutingModule } from './app-routing.module';
+
+// import { ErrorFourZeroFourComponent } from './errors/error-four-zero-four/error-four-zero-four.component';
+// import { AppComponent } from './app.component';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { GalleryService } from './services/gallery.services';
+// import { HighlightPipe } from './pipes/highlight.pipe';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     ErrorFourZeroFourComponent,
+//     HighlightPipe
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     BrowserAnimationsModule
+//   ],
+//   providers: [GalleryService],
+//   bootstrap: [AppComponent]
+// })
+
+// export class AppModule { }
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -7,21 +33,20 @@ import { ErrorFourZeroFourComponent } from './errors/error-four-zero-four/error-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GalleryService } from './services/gallery.services';
-import { HighlightPipe } from './pipes/highlight.pipe';
+import { HighlightPipe } from './works/components/design/highlight.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ErrorFourZeroFourComponent,
-    HighlightPipe
+    HighlightPipe,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [GalleryService],
   bootstrap: [AppComponent]
 })
-
 export class AppModule { }
