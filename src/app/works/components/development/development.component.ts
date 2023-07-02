c098860a31c46a69709025de30b646af2592da71
SBellido <sebastianbellidodg@gmail.com>
2023-07-02 15:48:06 +0200

    Completed texts

--- src/app/works/components/development/development.component.ts
+++ src/app/works/components/development/development.component.ts
@@ -8,12 +8,14 @@ import { Card } from '../../../models/models';
 })
 export class DevelopmentComponent implements OnInit {
 
- 
+  public area: string = 'desarrollo';
+
+
   cards: Card[] = [
     {
       id: 1,
-      tittle: 'custom software',
-      text: 'Is a practice that focuses on creating tailored digital solutions to meet the specific needs of a business or client. Unlike off-the-shelf or commercial software, custom software is designed to adapt to the unique requirements of an organization, enabling greater efficiency and productivity. This personalized approach involves detailed analysis of the client´s processes and requirements, followed by the design, development, and deployment of a bespoke technological solution. Custom software can encompass a wide range of applications, including enterprise systems, management platforms, e-commerce solutions, data analysis tools, and more. The key to successful custom software lies in its ability to deliver a precise and scalable solution, tailored to the needs and goals of each client.',
+      tittle: 'software a medida',
+      text: 'Práctica que se centra en la creación de soluciones digitales a medida para satisfacer las necesidades específicas de una empresa o cliente. A diferencia del software comercial o comercial, el software personalizado está diseñado para adaptarse a los requisitos únicos de una organización, lo que permite una mayor eficiencia y productividad. Este enfoque personalizado implica un análisis detallado de los procesos y requisitos del cliente, seguido del diseño, desarrollo e implementación de una solución tecnológica a medida. El software personalizado puede abarcar una amplia gama de aplicaciones, incluidos sistemas empresariales, plataformas de gestión, soluciones de comercio electrónico, herramientas de análisis de datos y más. La clave para el éxito del software personalizado radica en su capacidad para ofrecer una solución precisa y escalable, adaptada a las necesidades y objetivos de cada cliente.',
       image: '../../../../assets/images/works/DG.png',
       showFullText: false,
       truncatedText: '',
@@ -21,15 +23,15 @@ export class DevelopmentComponent implements OnInit {
     {
       id: 2,
       tittle: 'webs & apps',
-      text: 'Are crucial activities in the digital world. Web design focuses on creating attractive and functional visual interfaces for websites, while app development focuses on creating interactive programs and functionalities for mobile devices and other platforms. Both processes require detailed analysis of client requirements, planning of structure and navigation, design of intuitive and appealing interfaces, and implementation of custom functionalities. The goal is to provide users with a smooth and satisfying experience when interacting with websites and apps, whether it´s for obtaining information, conducting transactions, or utilizing specific services.',
+      text: 'Hoy Son cruciales en el mundo digital. El diseño web se centra en la creación de interfaces visuales atractivas y funcionales para sitios web, mientras que el desarrollo de aplicaciones se centra en la creación de programas interactivos y funcionalidades para dispositivos móviles y otras plataformas. Ambos procesos requieren un análisis detallado de los requisitos del cliente, planificación de la estructura y la navegación, diseño de interfaces intuitivas y atractivas, e implementación de funcionalidades personalizadas. El objetivo es proporcionar a los usuarios una experiencia fluida y satisfactoria al interactuar con sitios web y aplicaciones, ya sea para obtener información, realizar transacciones o utilizar servicios específicos.',
       image: '../../../../assets/images/works/DG.png',
       showFullText: false,
       truncatedText: '',
     }, 
     {
       id: 3,
-      tittle: 'e-commerce',
-      text:'Is a design and animation technique that combines graphic elements, movement and narrative to create dynamic and attractive visual content. Through the use of visual effects, fluid transitions and captivating animations, motion graphics seeks to communicate effectively and memorably. This discipline is applied in various media, such as promotional videos, presentations, advertising and audiovisual content in general. The objective of motion graphics is to convey messages in a clear, entertaining and persuasive way, capturing the attention of the viewer and generating visual impact. By combining graphic design, animation and sound, motion graphics allows you to tell captivating visual stories that stay in the minds of the audience.',
+      tittle: 'tiendas en línea',
+      text:'Plataformas digitales que permiten a los usuarios comprar productos o servicios de forma conveniente y segura desde la comodidad de sus hogares. Estas plataformas ofrecen una amplia variedad de productos, opciones de pago seguras y procesos de compra sencillos. En Mabell Studio, nos especializamos en el desarrollo y diseño de tiendas en línea personalizadas que brindan una experiencia de compra fluida y atractiva para los usuarios. Nuestro enfoque se basa en la creación de interfaces intuitivas, diseños atractivos y funcionalidades avanzadas que optimizan la experiencia del usuario y generan resultados comerciales exitosos.',
       image: '../../../../assets/images/works/DG.png',
       showFullText: false,
       truncatedText: '',
