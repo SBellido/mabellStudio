6747dc7486a06b187c63d786f167d13b3cdbdd24
SBellido <sebastianbellidodg@gmail.com>
2023-07-05 11:33:21 +0200

    Updated dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,3 @@
-// /* Angular */
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { AppRoutingModule } from './app-routing.module';
-
-// import { ErrorFourZeroFourComponent } from './errors/error-four-zero-four/error-four-zero-four.component';
-// import { AppComponent } from './app.component';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { GalleryService } from './services/gallery.services';
-// import { HighlightPipe } from './pipes/highlight.pipe';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     ErrorFourZeroFourComponent,
-//     HighlightPipe
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule,
-//     BrowserAnimationsModule
-//   ],
-//   providers: [GalleryService],
-//   bootstrap: [AppComponent]
-// })
-
-// export class AppModule { }
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
